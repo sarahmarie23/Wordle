b7746f146d62f110d2ebef8b61437bad078c4969
Sarah Jimenez <57870970+sarahmarie23@users.noreply.github.com>
2022-05-01 15:54:18 -0700

    Working text version

--- playWordle.py
+++ playWordle.py
@@ -1,54 +1,34 @@
 import wordle as w
 
-# defineGlobals: int, int/string -> null
-# purpose: keeps track of the game's settings
-#     if the user changes the default
-
-##secretWord = "panda"
-##global guesses
-##guesses = []
-##global maxGuesses
-##maxGuesses = 6
-##
-##def defineGlobals(wordLen = 5, diff = 1):
-##    global wordLength
-##    wordLength = wordLen
-##    global difficulty
-##    difficulty = diff
-##    global maxGuesses
-##    maxGuesses = 6
-##    global secretWord
-##    secretWord = ""
-##    global guesses
-##    guesses = []
-##    global gameOver
-##    gameOver = False
-##    global numGuesses
-##    numGuesses = 0
-
 
 
 def playWordle():
     currentGuesses = 0
-    w.guesses.clear()
-    while (currentGuesses < 6):
+    secret_word = w.getWord()
+    gameWon = False
+    
+    while (currentGuesses < 6 and not gameWon):
         
         guess = input("Guess a word: ")
 
         while(w.isGuessValid(guess) is not True):
             print(w.isGuessValid(guess))
             guess = input("Guess a word: ")
+
+        w.clearConsole()
             
         w.analyzeGuess(guess)
         w.displayGrid()
-        
-        if w.isCorrectGuess(guess):
-            print("You win!")
-            replay = input("Play again? y/n ")
-            if replay is "y":
-                playWordle()
-            break
-        currentGuesses +=1
+        gameWon = w.isCorrectGuess(guess)
 
-    print("Sorry, the answer was: ", w.secretWord)
+        currentGuesses +=1
         
+        
+    if gameWon:
+        print("You win!")
+    else:
+        print("Sorry, the answer was: ", w.secret_word)
+
+    w.gameOver()
+    if w.playAgain:
+        playWordle()
