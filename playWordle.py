702a4360b25d14807bada8376401248b53f14a7b
Sarah Jimenez <57870970+sarahmarie23@users.noreply.github.com>
2022-05-01 12:03:23 -0700

    Game loop updates

--- playWordle.py
+++ playWordle.py
@@ -4,36 +4,51 @@ import wordle as w
 # purpose: keeps track of the game's settings
 #     if the user changes the default
 
-secretWord = "start"
-global guesses
-guesses = []
-global maxGuesses
-maxGuesses = 6
-
-def defineGlobals(wordLen = 5, diff = 1):
-    global wordLength
-    wordLength = wordLen
-    global difficulty
-    difficulty = diff
-    global maxGuesses
-    maxGuesses = 6
-    global secretWord
-    secretWord = ""
-    global guesses
-    guesses = []
-    global gameOver
-    gameOver = False
-    global numGuesses
-    numGuesses = 0
+##secretWord = "panda"
+##global guesses
+##guesses = []
+##global maxGuesses
+##maxGuesses = 6
+##
+##def defineGlobals(wordLen = 5, diff = 1):
+##    global wordLength
+##    wordLength = wordLen
+##    global difficulty
+##    difficulty = diff
+##    global maxGuesses
+##    maxGuesses = 6
+##    global secretWord
+##    secretWord = ""
+##    global guesses
+##    guesses = []
+##    global gameOver
+##    gameOver = False
+##    global numGuesses
+##    numGuesses = 0
 
 
 
 def playWordle():
-
-    
-    while (len(guesses) < maxGuesses):
-        curr_row = w.emptyRow()
-        print(curr_row)
+    currentGuesses = 0
+    w.guesses.clear()
+    while (currentGuesses < 6):
+        
         guess = input("Guess a word: ")
-        w.storeGuesses(guesses, guess)
-        w.displayGrid(guesses, maxGuesses)
+
+        while(w.isGuessValid(guess) is not True):
+            print(w.isGuessValid(guess))
+            guess = input("Guess a word: ")
+            
+        w.analyzeGuess(guess)
+        w.displayGrid()
+        
+        if w.isCorrectGuess(guess):
+            print("You win!")
+            replay = input("Play again? y/n ")
+            if replay is "y":
+                playWordle()
+            break
+        currentGuesses +=1
+
+    print("Sorry, the answer was: ", w.secretWord)
+        
